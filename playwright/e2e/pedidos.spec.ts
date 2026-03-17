842e96a16f42f7a802475b22730468c528adb981
MariaLuizaRBrito <malubrito2012@gmail.com>
2026-03-17 14:42:51 -0300

    isolando dados + cenário de erro

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -3,6 +3,9 @@ import { test, expect } from '@playwright/test'
 ///AAA - Arrange, Act, Assert
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  //Test Data
+  const order = 'VLO-XAPMM6'
+
   //Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
@@ -10,19 +13,45 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   //Act
-  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-XAPMM6')
-  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
   //Assert
-  // opção 1
-  // const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-XAPMM6"]')
-  // await expect(orderCode).toBeVisible()
-
-  // opção 2
   const containerPedido = page.getByRole('paragraph')
                               .filter({hasText: /^Pedido$/})
                               .locator('..') // sobe para o elemento pai
 
-  await expect(containerPedido).toContainText('VLO-XAPMM6', {timeout: 10000})
+  await expect(containerPedido).toContainText(order, {timeout: 10000})
   await expect(page.getByText('APROVADO')).toBeVisible()
+})
+
+test('Deve exibir mensagem quando o pedido não é encontrado', async ({page})=> {
+
+  const order = 'VLO-ABC123'
+
+  //Arrange
+  await page.goto('http://localhost:5173/')
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  //Act
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+  //Assert
+  //const title = page.getByRole('heading', {name: 'Pedido não encontrado', level:3})
+  // await expect(title).toBeVisible()
+  //opção 1 - Xpath
+  // const mensagem = page.locator('//p[text()="Verifique o número do pedido e tente novamente"]')
+  //opção 2 - p + hasText
+  // const mensagem = page.locator('p', {hasText: 'Verifique o número do pedido e tente novamente'})
+  // await expect(mensagem).toBeVisible()
+
+  //opção 3 - toMatchAriaSnapshot
+  await expect(page.locator('#root')).toMatchAriaSnapshot(`
+    - img
+    - heading "Pedido não encontrado" [level=3]
+    - paragraph: Verifique o número do pedido e tente novamente
+    `)
 })
